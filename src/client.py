f7127d4cc74d8328a597589f3bd1ee8d86441300
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2021-03-21 19:43:13 +0530

    changes

--- src/client.py
+++
@@ -1,5 +0,0 @@
-import xmlrpc.client
-
-with xmlrpc.client.ServerProxy("http://10.11.0.94:49664/") as proxy:
-    print("3 is even: %s" % str(proxy.is_even(3)))
-    print("100 is even: %s" % str(proxy.is_even(100)))
