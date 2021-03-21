ab5bc62aa652a9db941eb6008ba0ab5c6f1d6ae5
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2021-03-21 09:47:13 +0530

    test changes added to client and server

--- src/client.py
+++ src/client.py
@@ -1,5 +1,5 @@
 import xmlrpc.client
 
-with xmlrpc.client.ServerProxy("http://10.11.1.114:8000/") as proxy:
+with xmlrpc.client.ServerProxy("http://10.11.0.94:49664/") as proxy:
     print("3 is even: %s" % str(proxy.is_even(3)))
     print("100 is even: %s" % str(proxy.is_even(100)))
--- src/server.py
+++ src/server.py
@@ -3,7 +3,7 @@ from xmlrpc.server import SimpleXMLRPCServer
 def is_even(n):
     return n % 2 == 0
 
-server = SimpleXMLRPCServer(("10.11.1.114", 8000))
+server = SimpleXMLRPCServer(("localhost", 49664))
 print("Listening on port 8000...")
 server.register_function(is_even, "is_even")
 server.serve_forever()
